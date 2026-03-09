23bfca3a205df3ad244aa243b61ae02ca18f1a4e
Kayla Emily <emilykayla097@gmail.com>
2026-03-09 18:51:31 +0800

    update app jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,9 +9,7 @@ function App() {
       {/* Spacer supaya konten tidak tertutup Navbar yang fixed */}
       <div className="pt-16">
         {/* Nanti kita lanjut bikin bagian Hero di sini */}
-        <div className="text-center py-20">
-          <h1 className="text-4xl font-bold text-gray-800">Navbar Berhasil Terpasang!</h1>
-        </div>
+
       </div>
     </div>
   );
